2f179beb52d1572900d94a43de711f3ea0c71e6d
Arthur Ford <adf.ford@gmail.com>
2022-01-22 15:15:24 -0500

    Initial Commit

---
+++ LightsOut/LightsOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsOutApp.swift
+//  LightsOut
+//
+//  Created by Arthur Ford on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LightsOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
